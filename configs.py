da239e30617413ee20fabd3ea0bdc9ad537a6465
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-02 13:25:44 -0400

    put configs inside configs dict

--- configs.py
+++ configs.py
@@ -4,6 +4,22 @@ from pathlib import Path
 from hyperopt import hp
 
 
+def get_config(name):
+    path = Path("configs", name).with_suffix(".json")
+    if path.exists():
+        with path.open() as f:
+            config = json.load(f)
+            del config["use_tune"]
+            del config["max_timesteps"]
+            return config
+    return configs[name]
+
+
+def copy_args(d, prefix):
+    for k, v in d.items():
+        yield prefix + k, v
+
+
 def small_values(start, stop):
     return [j for i in range(start, stop) for j in ((10 ** -i), 5 * (10 ** -i))]
 
@@ -16,6 +32,7 @@ def big_values(start, stop):
     return [j for i in range(start, stop) for j in ((10 ** i), 5 * (10 ** i))]
 
 
+debug4 = get_config("debug4")
 search = dict(
     batch_size=hp.choice("batch_size", medium_values(6, 10)),
     expl_noise=hp.choice("expl_noise", small_values(1, 3)),
@@ -25,41 +42,6 @@ search = dict(
     seed=hp.randint("seed", 20),
     start_timesteps=hp.choice("start_timesteps", big_values(0, 2)),
 )
-
-pendulum = {
-    "batch_size": 256,
-    "discount": 0.99,
-    "env_id": "Pendulum-v0",
-    "eval_freq": 5000.0,
-    "expl_noise": 0.01,
-    "lr": 0.001,
-    "max_timesteps": 15000,
-    "noise_clip": 0.01,
-    "policy": "TD3",
-    "policy_freq": 1,
-    "policy_noise": 0.2,
-    "replay_size": 200000,
-    "seed": 3,
-    "start_timesteps": 10,
-}
-
-
-def copy_args(d, prefix):
-    for k, v in d.items():
-        yield prefix + k, v
-
-
-debug4 = get_config("debug4")
-
-outer_search = dict(
-    max_timesteps=hp.choice("max_timesteps", [500, 1000]),
-    update_freq=hp.choice("update_freq", [10, 20, 30, 40, 50]),
-    context_length=hp.choice("context_length", [10, 50, 100, 200]),
-    **dict(copy_args(search, "outer_")),
-    **dict(copy_args(debug4, "inner_")),
-)
-
-# TODO
 single = {
     "batch_size": 128,
     "discount": 0.99,
@@ -74,22 +56,39 @@ single = {
     "seed": 8,
     "start_timesteps": 5,
 }
-
-# TODO
-double = dict(
-    **dict(copy_args(single, "outer_")),
-    **dict(copy_args(single, "inner_")),
-    update_freq=20,
-    context_length=200,
+configs = dict(
+    search=search,
+    pendulum={
+        "batch_size": 256,
+        "discount": 0.99,
+        "env_id": "Pendulum-v0",
+        "eval_freq": 5000.0,
+        "expl_noise": 0.01,
+        "lr": 0.001,
+        "max_timesteps": 15000,
+        "noise_clip": 0.01,
+        "policy": "TD3",
+        "policy_freq": 1,
+        "policy_noise": 0.2,
+        "replay_size": 200000,
+        "seed": 3,
+        "start_timesteps": 10,
+    },
+    debug4=debug4,
+    outer_search=dict(
+        max_timesteps=hp.choice("max_timesteps", [500, 1000]),
+        update_freq=hp.choice("update_freq", [10, 20, 30, 40, 50]),
+        context_length=hp.choice("context_length", [10, 50, 100, 200]),
+        **dict(copy_args(search, "outer_")),
+        **dict(copy_args(debug4, "inner_")),
+    ),
+    # TODO
+    single=single,
+    # TODO
+    double=dict(
+        **dict(copy_args(single, "outer_")),
+        **dict(copy_args(single, "inner_")),
+        update_freq=20,
+        context_length=200,
+    ),
 )
-
-
-def get_config(name):
-    path = Path("configs", name).with_suffix(".json")
-    if path.exists():
-        with path.open() as f:
-            config = json.load(f)
-            del config["use_tune"]
-            del config["max_timesteps"]
-            return config
-    return eval(name)
