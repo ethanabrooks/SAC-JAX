f20b0fbae56600cdf2183b9475251a06586774a5
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-03 12:00:00 -0400

    back to debugEnv

--- configs.py
+++ configs.py
@@ -84,11 +84,11 @@ l2b_search = dict(
     context_length=hp.choice("context_length", [40, 50, 60]),
     sample_done_prob=hp.choice("sample_done_prob", [0.1, 0.3, 0.5]),
     update_freq=hp.choice("update_freq", [40, 50, 75, 100]),
-    **dict(copy_args(pendulum, "inner_")),
+    **dict(copy_args(debug4, "inner_")),
     **outer_search,
 )
 l2b_search.update(
-    inner_max_timesteps=hp.choice("inner_max_timesteps", [5000, 6000, 7000])
+    inner_max_timesteps=hp.choice("inner_max_timesteps", [500, 1000, 1500])
 )
 double = dict(
     **dict(copy_args(single, "outer_")),
