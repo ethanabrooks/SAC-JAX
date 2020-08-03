2f1c724bf9bd10b58f4cabb1b7d978452dc71e38
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-03 19:17:57 -0400

    just use encoder to split

--- l2b_agent.py
+++ l2b_agent.py
@@ -16,57 +16,51 @@ class ContextEncoder(hk.Module):
         self.obs_size = obs_size
 
     def __call__(self, obs: np.ndarray) -> jnp.DeviceArray:
-        encoder = hk.Sequential(
-            [
-                hk.Linear(
-                    256,
-                    w_init=hk.initializers.VarianceScaling(
-                        scale=2.0, distribution="uniform"
-                    ),
-                ),
-                jax.nn.relu,
-                hk.Linear(
-                    256,
-                    w_init=hk.initializers.VarianceScaling(
-                        scale=2.0, distribution="uniform"
-                    ),
-                ),
-            ]
-        )
-        d = obs.shape[-1]
-        unsqueeze = obs.reshape(-1, d)
-        n = unsqueeze.shape[0]
-        s, c = jnp.hsplit(unsqueeze, [self.obs_size])
-        c = c.reshape(n, self.context_length, -1)
-        e = encoder(c)
-        e = e.prod(axis=1)
-        se = jnp.concatenate([s, e], axis=-1)
-        return se.reshape(*obs.shape[:-1], -1)
+        # encoder = hk.Sequential(
+        #     [
+        #         hk.Linear(
+        #             256,
+        #             w_init=hk.initializers.VarianceScaling(
+        #                 scale=2.0, distribution="uniform"
+        #             ),
+        #         ),
+        #         jax.nn.relu,
+        #         hk.Linear(
+        #             256,
+        #             w_init=hk.initializers.VarianceScaling(
+        #                 scale=2.0, distribution="uniform"
+        #             ),
+        #         ),
+        #     ]
+        # )
+        s, c = jnp.split(obs, [self.obs_size], axis=-1)
+        # c = c.reshape(obs.shape[0], self.context_length, -1)
+        # e = encoder(c)
+        # e = e.prod(axis=1)
+        # se = jnp.concatenate([s, e], axis=-1)
+        # return se.reshape(*obs.shape[:-1], -1)
+        return s
 
 
 class Actor(networks.Actor):
     def __init__(self, obs_size: int, context_length: int, **kwargs):
         super(Actor, self).__init__(**kwargs)
-        self.obs_size = obs_size
-        # self.encoder = ContextEncoder(obs_size, context_length)
+        self.encoder = ContextEncoder(obs_size, context_length)
 
     def __call__(self, obs: np.ndarray) -> jnp.DeviceArray:
-        obs, _ = jnp.split(obs, [self.obs_size], axis=-1)
-        # obs = self.encoder(obs)
+        obs = self.encoder(obs)
         return super().__call__(obs)
 
 
 class Critic(networks.Critic):
     def __init__(self, obs_size: int, context_length: int):
         super(Critic, self).__init__()
-        self.obs_size = obs_size
-        # self.encoder = ContextEncoder(obs_size, context_length)
+        self.encoder = ContextEncoder(obs_size, context_length)
 
     def __call__(
         self, obs: np.ndarray, action: np.ndarray
     ) -> Tuple[jnp.DeviceArray, jnp.DeviceArray]:
-        # obs = self.encoder(obs)
-        obs, _ = jnp.split(obs, [self.obs_size], axis=-1)
+        obs = self.encoder(obs)
         return super().__call__(obs, action)
 
 
