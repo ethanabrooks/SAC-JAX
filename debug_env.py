abd478263992adff4e49f762f7616bdb7c17cceb
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-04 16:08:38 -0400

    fix seeding in debug_env

--- debug_env.py
+++ debug_env.py
@@ -12,17 +12,16 @@ class DebugEnv(gym.Env):
     def __init__(self):
         # , levels: int, dim: int, tol: float, seed: int):
         # self.tol = tol
-        # self.embeddings = np.random.random((levels, dim))
-        # self.acceptable = np.random.random(levels)
-        self.acceptable = np.random.random()
         self.iterator = None
         self.random, _ = np_random(0)
+        self.acceptable = self.random.random()
         self.observation_space = gym.spaces.Box(low=np.zeros(1), high=np.zeros(1))
         self.action_space = gym.spaces.Box(low=np.zeros(1), high=np.ones(1))
         self._max_episode_steps = 2
 
     def seed(self, seed=None):
         self.random, _ = np_random(seed)
+        self.acceptable = self.random.random()
 
     def generator(self):
         s = np.zeros(1)
