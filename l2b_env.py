ac0ae34971bdffa18fd4539b72a7a410fc5dbfe0
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-04 16:53:49 -0400

    fix env_id in l2b_env

--- l2b_env.py
+++ l2b_env.py
@@ -32,7 +32,7 @@ class CatObsSpace(gym.ObservationWrapper):
 
 class L2bEnv(Trainer, gym.Env):
     def __init__(self, update_freq, context_length, alpha, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, env_id=None, **kwargs)
         self.alpha = alpha
         self.update_freq = update_freq
         self.context_length = context_length
