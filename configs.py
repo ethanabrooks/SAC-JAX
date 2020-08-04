7b31240b7d43d909e3962ecb48a0921cd13ff175
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-04 13:04:22 -0400

    try doubling update_freq

--- configs.py
+++ configs.py
@@ -83,7 +83,7 @@ l2b_search.update(
 debug_l2b = dict(
     context_length=2,
     sample_done_prob=0,
-    update_freq=1,  # TODO
+    update_freq=2,  # TODO
     **dict(copy_args(pendulum, "inner_")),
     **dict(copy_args(pendulum, "outer_")),
 )
