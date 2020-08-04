0f43c3357e2afdb00cb4b360a6390aec13831b0c
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-04 17:17:58 -0400

    fix the final_reward bug

--- l2b_trainer.py
+++ l2b_trainer.py
@@ -17,7 +17,9 @@ class L2bTrainer(Trainer):
         super().__init__(**outer_args, env_id=env_id)
 
     def report(self, **kwargs):
-        super().report(**{"outer_" + k: v for k, v in kwargs.items()})
+        super().report(
+            **{k if k == "final_reward" else "outer_" + k: v for k, v in kwargs.items()}
+        )
 
     @classmethod
     def run(cls, config: dict):
