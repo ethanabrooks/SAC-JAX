254e88d06eb769b2c099afa4682393b2973a063c
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-06 18:46:13 -0400

    add RUN_KILL_LABEL to env

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 export CONTAINER_VOLUME="/root/ray_results"
+export RUN_KILL_LABEL="RUN"
 export RUN_DB_PATH="$HOME/runs/td3"
 export RUN_IMAGE_BUILD_PATH="$HOME/TD3-JAX/"
 export RUN_DOCKERFILE_PATH="$RUN_IMAGE_BUILD_PATH/Dockerfile"
@@ -8,4 +9,4 @@ export RUN_IMAGE='td3'
 export RUN_CONFIG_SCRIPT='config_script.py'
 export RUN_CONFIG_SCRIPT_INTERPRETER='python3'
 export RUN_CONFIG_SCRIPT_INTERPRETER_ARGS='-c'
-export DOCKER_RUN_COMMAND='docker run -d --rm --gpus all -it'
+export DOCKER_RUN_COMMAND='docker run --rm --gpus all -itd --label RUN'
