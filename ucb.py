e3be25c0df9d1b89e3e2293a8f442927522d384f
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-09 11:39:52 -0400

    1. break ties randomly in argmax. 2. sample each arm once initially to eliminate nan

--- ucb.py
+++ ucb.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 """
 Created on Tue Sep 11 17:00:29 2018
@@ -7,32 +7,46 @@ Created on Tue Sep 11 17:00:29 2018
 @author: Mohammad Doosti Lakhani
 """
 
-# Importing dataset
-dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
 
-# Implementing Upper Bound Confidence
-m, n = dataset.shape
+def argmax(x: np.ndarray):
+    return int(np.random.choice(np.arange(x.size)[x == x.max(initial=-np.inf)]))
 
-ad_selected = np.zeros(m)  # selected ads by users
 
-N = np.zeros(n, dtype=np.float16)  # number of selection of ad i
-R = np.zeros(n, dtype=np.float16)  # sum of reward of ad i
+def main():
+    # Importing dataset
+    dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
 
-total_reward = 0
+    # Implementing Upper Bound Confidence
+    m, n = dataset.shape
 
-# implementation in vectorized form
-for i in range(0, m):
-    r = R / N
-    delta = np.sqrt(3 / 2 * np.log(i + 1) / N)
-    upper_bound = r + delta
-    max_index = np.argmax(upper_bound)
-    ad_selected[i] = max_index
-    N[max_index] += 1
-    reward = dataset.values[i, max_index]
-    R[max_index] += reward
-    print(r.mean())
-    total_reward += reward
+    selections = np.zeros((m + 1, n), dtype=np.float16)  # number of selection of ad i
+    rewards = np.zeros((m + 1, n), dtype=np.float16)  # sum of reward of ad i
 
-# Visualizing selections
-# plt.hist(ad_selected)
-# plt.show()
+    # first sample each
+    selections[:n] = np.tril(np.ones((n, n)))
+    # # noinspection PyTypeChecker
+    np.fill_diagonal(rewards, np.diag(dataset.values))
+    rewards[:n] = np.cumsum(rewards[:n], axis=0)
+
+    # implementation in vectorized form
+    for i, (N0, R0, N1, R1), in enumerate(
+        zip(selections[n - 1 :], rewards[n - 1 :], selections[n:], rewards[n:])
+    ):
+        r = R0 / N0
+        delta = np.sqrt(3 / 2 * np.log(i + 1) / N0)
+        upper_bound = r + delta
+        max_index = argmax(upper_bound)
+        N1[:] = N0
+        N1[max_index] += 1
+        reward = dataset.values[i, max_index]
+        R1[:] = R0
+        R1[max_index] += reward
+        print(r[max_index])
+
+    # Visualizing selections
+    # plt.hist(ad_selected)
+    # plt.show()
+
+
+if __name__ == "__main__":
+    main()
